220f5d57f8967c01db4538828eada43f2dc82857
gichukiwairua <gichukisimon@gmail.com>
2025-08-02 06:50:28 +0300

    fixed vite

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -3,6 +3,10 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL?: string;
   readonly VITE_APP_NAME?: string;
+  readonly DEV: boolean;
+  readonly PROD: boolean;
+  readonly SSR: boolean;
+  readonly MODE: string;
 }
 
 interface ImportMeta {
